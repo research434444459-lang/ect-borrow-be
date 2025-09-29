78be39fed4dcc767408d153ff13307c609b48e48
research434.444.459 <research434.444.459@gmail.com>
2025-09-29 23:33:10 +0700

    connect fe

--- internal/config/config.go
+++ internal/config/config.go
@@ -24,7 +24,7 @@ func Load() Config {
 		SheetTabRequests:  valueOr("SHEET_TAB_REQUESTS", "การตอบแบบฟอร์ม 1"),
 		Subtitle:          valueOr("DEVICES_SUBTITLE", "ดูรายการอุปกรณ์"),
 		ReadyStatus:       valueOr("READY_STATUS", "พร้อมใช้งาน"),
-		FrontendOrigin:    valueOr("FRONTEND_ORIGIN", "http://localhost:4200"),
+		FrontendOrigin:    valueOr("FRONTEND_ORIGIN", "https://ect-borrow.pages.dev"),
 	}
 }
 
